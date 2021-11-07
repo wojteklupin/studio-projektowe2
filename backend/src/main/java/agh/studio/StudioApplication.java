adbc842b3da1b84f3cf270b3487e13a08abeec7f
kamilr123 <kamilrr1722@gmail.com>
2021-11-07 13:03:43 +0100

    add backend skelethon

---
+++ backend/src/main/java/agh/studio/StudioApplication.java
@@ -0,0 +1,13 @@
+package agh.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/agh/studio/StudioApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
