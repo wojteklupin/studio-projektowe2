9405e46db4ca239f6e8270a0086ea176d2937f3e
Wojciech Łupina <wojteklupin@gmail.com>
2021-11-11 12:46:07 +0100

    Added frontend skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
