7d61e1c29a1722830e81551dce0664dee03eb78a
Wojciech Łupina <wojteklupin@gmail.com>
2021-12-14 16:00:11 +0100

    More stats not work - not with filters :(

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import { GoogleCharts } from 'google-charts'
+import VueGoogleCharts from 'vue-google-charts'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueGoogleCharts)
 
 new Vue({
   vuetify,
