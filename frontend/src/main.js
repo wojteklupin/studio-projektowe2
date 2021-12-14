2705d52c0445a9e010ec21b4711144e405e21156
Wojciech Łupina <wojteklupin@gmail.com>
2021-12-14 12:55:15 +0100

    Change from vue-google-charts to google-charts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import VueGoogleCharts from 'vue-google-charts'
+import { GoogleCharts } from 'google-charts'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(VueGoogleCharts)
 
 new Vue({
   vuetify,
--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -5,17 +5,29 @@
                 <v-container>
                     <v-row>
                         <v-col cols="6">
-                            <GChart
+                            <div id="voivodshipChart"></div>
+                        </v-col>
+                        <v-col cols="6">
+                            <!-- <GChart
                                 type="ColumnChart"
-                                :data="chartData"
-                                :options="chartOptions"
-                            />
+                                :data="fuelChartData"
+                                :options="fuelChartOptions"
+                            /> -->
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="6">
+                            <!-- <GChart
+                                type="ColumnChart"
+                                :data="bodyChartData"
+                                :options="bodyChartOptions"
+                            /> -->
                         </v-col>
                         <v-col cols="6">
                             <GChart
                                 type="ColumnChart"
-                                :data="chartData"
-                                :options="chartOptions"
+                                :data="makeChartData"
+                                :options="makeChartOptions"
                             />
                         </v-col>
                     </v-row>
@@ -23,22 +35,135 @@
                         <v-col cols="6">
                             <GChart
                                 type="ColumnChart"
-                                :data="chartData"
-                                :options="chartOptions"
+                                :data="powerChartData"
+                                :options="powerChartOptions"
                             />
                         </v-col>
                         <v-col cols="6">
                             <GChart
                                 type="ColumnChart"
-                                :data="chartData"
-                                :options="chartOptions"
+                                :data="capacityChartData"
+                                :options="capacityChartOptions"
+                            />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12">
+                            <GChart
+                                type="ColumnChart"
+                                :data="priceChartData"
+                                :options="priceChartOptions"
                             />
                         </v-col>
                     </v-row>
                 </v-container>
             </v-col>
 
-            <v-col cols="2"> Filtry: </v-col>
+            <v-col cols="2">
+                <v-radio-group v-model="vehicleCategory">
+                    <template v-slot:label>
+                        <div>Kategoria:</div>
+                    </template>
+                    <v-radio
+                        key="0"
+                        label="Samochody osobowe"
+                        value="cars"
+                    ></v-radio>
+                    <v-radio
+                        key="1"
+                        label="Motocykle"
+                        value="motorcycles"
+                    ></v-radio>
+                </v-radio-group>
+
+                <v-select
+                    v-model="sortBy"
+                    :items="sortByOptions"
+                    label="Sortuj od"
+                    dense
+                ></v-select>
+                <v-select
+                    v-model="brand"
+                    :items="brands"
+                    label="Marka pojazdu"
+                    dense
+                ></v-select>
+                Zakres cen:
+                <v-container>
+                    <v-row>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="priceLower"
+                                single-line
+                                label="Od"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="priceUpper"
+                                single-line
+                                label="Do"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-container>
+                Rok produkcji:
+                <v-container>
+                    <v-row>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="yearLower"
+                                single-line
+                                label="Od"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="yearUpper"
+                                single-line
+                                label="Do"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-container>
+                Przebieg pojazdu:
+                <v-container>
+                    <v-row>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="mileageLower"
+                                single-line
+                                label="Od"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="mileageUpper"
+                                single-line
+                                label="Do"
+                                dense
+                                outlined
+                                type="number"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -46,19 +171,102 @@
 <script>
 export default {
     data: () => ({
-        chartData: [
-            ["Year", "Sales", "Expenses", "Profit"],
-            ["2014", 1000, 400, 200],
-            ["2015", 1170, 460, 250],
-            ["2016", 660, 1120, 300],
-            ["2017", 1030, 540, 350],
+        vehicleCategory: "cars",
+        priceLower: null,
+        priceUpper: null,
+        yearLower: null,
+        yearUpper: null,
+        mileageLower: null,
+        mileageUpper: null,
+        sortBy: "Najnowszych",
+        brand: null,
+        brands: [],
+        sortByOptions: ["Najnowszych", "Najtańszych", "Najdroższych"],
+        voivodshipChartData: [
+            ["Województwo", "Ilość ogłoszeń"]
         ],
-        chartOptions: {
+        voivodshipChartOptions: {
+            chart: {
+                title: "Company Performance"
+            },
+            height: 400,
+        },
+        fuelChartData: [
+            ["Paliwo", "Ilość ogłoszeń"]
+        ],
+        fuelChartOptions: {
+            chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017",
+            },
+        },
+        bodyChartData: [
+            ["Nadwozie", "Ilość ogłoszeń"]
+        ],
+        bodyChartOptions: {
+            chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017",
+            },
+        },
+        makeChartData: [
+            ["Marka", "Ilość ogłoszeń"]
+        ],
+        makeChartOptions: {
+            chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017",
+            },
+        },
+        powerChartData: [
+            ["Moc silnika [KM]", "Ilość ogłoszeń"]
+        ],
+        powerChartOptions: {
+            chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017",
+            },
+        },
+        capacityChartData: [
+            ["Pojemność silnika [cm3]", "Ilość ogłoszeń"]
+        ],
+        capacityChartOptions: {
+            chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017",
+            },
+        },
+        priceChartData: [
+            ["Cena", "Ilość ogłoszeń"]
+        ],
+        priceChartOptions: {
             chart: {
                 title: "Company Performance",
                 subtitle: "Sales, Expenses, and Profit: 2014-2017",
             },
         },
     }),
+    created() {
+        const fetchStats = async function(vm) {
+            let response = await fetch(`http://localhost:8080/cars/stats`)
+            let stats = await response.json()
+            for (let item of stats.voivodship) {
+                vm.voivodshipChartData.push([item.label, item.count])
+            }
+            for (let item of stats.fuel) {
+                vm.fuelChartData.push([item.label, item.count])
+            }
+            for (let item of stats.body) {
+                vm.bodyChartData.push([item.label, item.count])
+            }
+            for (let item of stats.make) {
+                vm.makeChartData.push([item.label, item.count])
+            }
+            for (let item of stats.power) {
+                vm.powerChartData.push([item.label, item.count])
+            }
+        }
+        fetchStats(this)
+    }
 };
 </script>
